6af9c6199d774391d67402b5dcb7d0dd715095ec
Dương Trí Dũng <crytv778@gmail.com>
2025-03-01 12:28:42 +0700

    add input, popover, and combobox components; update page with new UI elements and lottery data handling

--- src/app/travenhanh/page.tsx
+++ src/app/travenhanh/page.tsx
@@ -1,23 +1,84 @@
 "use client";
+import { Input } from "@/components/ui/input";
 import { getLotteryData } from "@/utils/fecht-lottery-api";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { mangDai } from "@/utils/mangDai";
+import { Combobox } from "@/components/ui/combobox";
+import Swal from "sweetalert2";
+import {
+  convertLotteryResultsNorthSide,
+  convertLotteryResultsSouthSide,
+} from "@/utils/convertLotteryResults";
+import {
+  findWinningPrizeNorthSide,
+  findWinningPrizeSouthSide,
+} from "@/utils/findWinningPrize";
 
 const TraVeNhanhPage = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState();
+  const [value, setValue] = useState("miba");
+  const [number, setNumber] = useState<string>("");
 
   useEffect(() => {
-    getLotteryData("miba").then((res) => {
+    getLotteryData(value).then((res) => {
       setData(res);
     });
-  }, []);
+  }, [value]);
+
+  const handleSearch = useCallback(() => {
+    if (!/^\d+$/.test(number)) {
+      Swal.fire({
+        title: "Vui lòng nhập đúng định dạng số",
+        icon: "info",
+        confirmButtonText: "Đóng",
+      });
+      return;
+    }
+
+    if (["mb", "mn", "mt"].includes(data.t.navCate)) {
+      const results = data.t.issueList.flatMap((item) => {
+        let lotteryResults = [];
+        const prizes = [];
+        if (data.t.navCate === "mb") {
+          lotteryResults = convertLotteryResultsNorthSide(item.detail);
+          prizes.push(...findWinningPrizeNorthSide(number, lotteryResults));
+        }
+        if (data.t.navCate === "mn" || data.t.navCate === "mt") {
+          lotteryResults = convertLotteryResultsSouthSide(item.detail);
+          prizes.push(...findWinningPrizeSouthSide(number, lotteryResults));
+        }
+        return prizes.length > 0
+          ? prizes.map(
+              (prize) =>
+                `Số ${number} trúng giải ${prize} ngày ${item.turnNum} đài ${data.t.name}`
+            )
+          : [];
+      });
+
+      Swal.fire({
+        title: "Kết quả trúng thưởng",
+        html:
+          results.length > 0
+            ? `<div style="text-align: left;">${results.join("<br>")}</div>`
+            : `Số ${number} không trúng giải nào`,
+        icon: results.length > 0 ? "success" : "info",
+        confirmButtonText: "Đóng",
+      });
+    }
+  }, [data, number]);
+
   return (
     <div>
-      {data && (
-        <div>
-          <h2>Dữ liệu từ API:</h2>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
+      <div className=" flex flex-col items-center justify-center h-96">
+        <div className="text-4xl">Tra vé nhanh 🚀</div>
+        <div>Một trang web giúp hỗ trợ tra cứu vé số nhanh hơn!</div>
+        <div className="mt-4 flex space-x-4">
+          <Combobox datas={mangDai} setValue={setValue} value={value} />
+          <Input type="text" onChange={(e) => setNumber(e.target.value)} />
+          <Button onClick={handleSearch}>Tìm kiếm</Button>
         </div>
-      )}
+      </div>
     </div>
   );
 };
---
+++ src/components/ui/combobox.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+type DataType = {
+  code: string;
+  name: string;
+};
+
+type ComboboxProps = {
+  datas?: DataType[];
+  setValue: (value: string) => void;
+  value: string;
+};
+
+export function Combobox({ datas = [], setValue, value }: ComboboxProps) {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value ? datas.find((data) => data.code === value)?.name : "Chọn đài"}
+          <ChevronsUpDown className="opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Đang chọn đài ...." className="h-9" />
+          <CommandList>
+            <CommandEmpty>Không tìm thấy đài này.</CommandEmpty>
+            <CommandGroup heading="Đài">
+              {datas.map((data, index) => (
+                <CommandItem
+                  key={data.code}
+                  value={data.code}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                  }}
+                >
+                  {data.name}
+                  <Check
+                    className={cn(
+                      "ml-auto",
+                      value === data.code ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
